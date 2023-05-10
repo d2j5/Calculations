7db5240d3d61d8fb9c027af6b239c606b0783118
Javier <d2j5@hotmail.com>
2023-05-09 22:02:29 -0400

    v1

--- src/PracticingLoops/TakeABreak.java
+++ src/PracticingLoops/TakeABreak.java
@@ -1,4 +1,15 @@
 package PracticingLoops;
-
+import java.util.Scanner;
 public class TakeABreak {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String response = "";
+
+        while (!response.equals("yes")) {
+            System.out.println("Do you want to take a break?");
+            response = input.nextLine();
+        }
+        System.out.println("Great! Take a break!");
+    }
+
 }
